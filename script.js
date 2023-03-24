2ee709d408bf236ba242ac1cd81ce5a136d06d65
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-03-25 00:00:36 +0100

    I have added functions for add, substract, multiply, and divide

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ let secondNumber = "";
 let clickedOperator = "";
 let storedNumber = "";
 let result = 0;
+
+// functions for add, subtract, multiply,  and divide.
+const add = function (a, b) {
+  result = a + b;
+  return result;
+};
+
+const subtract = function (a, b) {
+  result = a - b;
+  return result;
+};
+const multiply = function (a, b) {
+  result = a * b;
+  return result;
+};
+
+const divide = function (a, b) {
+  result = a / b;
+  return result;
+};
