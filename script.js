89818bd4db2f9fa0e0c4dccf020f24d0b30b7e2a
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-03-24 23:53:27 +0100

    added dom elements to my script.js file

---
+++ script.js
@@ -0,0 +1,9 @@
+// DOM //
+
+const output = document.querySelector(".output");
+const buttonNumbers = document.querySelectorAll(".numbers");
+const operators = document.querySelectorAll(".operators");
+const displayResult = document.createElement("span");
+const buttonEqual = document.querySelector(".equal");
+
+// DOM //
